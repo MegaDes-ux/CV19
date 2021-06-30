18887a0fa67b908ecd413210479ec0f5c13c9fbc
Detsrock <stoyko@rambler.ru>
2021-06-30 17:24:46 +0500

    Тестирование работы модели-представления главного окна

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CV19.ViewModels.Base;
 
 namespace CV19.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
 
+        private string _Title= "Анализ статистики CV19";
+        /// <summary> Заголовок окна </summary>
+        public string Title
+        {
+            get => _Title;
+            /*set
+             {
+                if (Equals(_Title, value)) return;
+                _Title = value;
+                OnPropertyChanged();
+            //Можно сократить в Set(ref _Title, value);
+            } */
+            //Можно сократить в 
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
     }
 }
