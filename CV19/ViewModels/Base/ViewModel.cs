2fd1a21de21371f5f531c36adb5ead1ea2bb273f
Detsrock <stoyko@rambler.ru>
2021-06-30 14:23:29 +0500

    Делаем модель представления

---
+++ CV19/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel
+using System.Runtime.CompilerServices
+
+namespace CV19.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+} 
