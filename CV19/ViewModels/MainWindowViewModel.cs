3674e7b4b069d6e9019aa24246367a30ce7a08df
Detsrock <stoyko@rambler.ru>
2021-07-05 15:15:42 +0500

    Привязка кнопок

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using CV19.Infrastructure.Commands;
 using CV19.ViewModels.Base;
 using CV19.Models;
@@ -14,13 +15,27 @@ namespace CV19.ViewModels
         #region TestDataPoint : IEnumerable<DataPoint> - Тестовый набор данных для визуализации
 
         ///<summary>Тестовый набор данных для визуализации</summary>
-        private IEnumerable<DataPoint> _TestDataPoint;
+        private IEnumerable<DataPoint> _TestDataPoints;
 
         ///<summary>Тестовый набор данных для визуализации</summary>
-        public IEnumerable<DataPoint> TestDataPoint
+        public IEnumerable<DataPoint> TestDataPoints
         {
-            get => _TestDataPoint;
-            set => Set(ref _TestDataPoint, value);
+            get => _TestDataPoints;
+            set => Set(ref _TestDataPoints, value);
+        }
+
+        #endregion
+
+        #region SelectedPageIndex : int - Номер выбранной вкладки
+
+        ///<summary>Номер выбранной вкладки</summary>
+        private int _SelectedPageIndex;
+
+        ///<summary>Номер выбранной вкладки</summary>
+        public int SelectedPageIndex
+        {
+            get => _SelectedPageIndex;
+            set => Set(ref _SelectedPageIndex, value);
         }
 
         #endregion
@@ -72,27 +87,48 @@ namespace CV19.ViewModels
         }
 
         #endregion
-    
+
+        #region ChangePageIndexCommand
+
+        public ICommand ChangePageIndexCommand { get; }
+
+        private bool CanChangePageIndexCommandExecute(object p) => _SelectedPageIndex >= 0;
+
+        private void OnChangePageIndexCommandExecuted(object p)
+        {
+            if (p is null) return;
+            SelectedPageIndex += Convert.ToInt32(p);
+        }
+
         #endregion
 
-    public MainWindowViewModel()  // конструктор 
+        #endregion
+
+        public MainWindowViewModel()  // конструктор 
         {
             #region Команды
 
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            ChangePageIndexCommand = new LambdaCommand(OnChangePageIndexCommandExecuted, CanChangePageIndexCommandExecute);
 
             #endregion
 
+            #region Тестовый график
+
             var data_points = new List<DataPoint>((int)(360 / 0.1));
-            for (var x = 0d; x < 360; x += 0.1)
+            for (var x = 0d; x <= 360; x += 0.1)
             {
                 const double TO_RAD = Math.PI / 180;
-                var y = Math.Sin(2 * Math.PI * x * TO_RAD);
+                var y = Math.Sin(x * TO_RAD);
 
                 data_points.Add(new DataPoint{ XValue = x, YValue = y});
             }
 
-            TestDataPoint = data_points;
+            TestDataPoints = data_points;
+
+            #endregion
+
+            //SelectedPageIndex = 1;
         }
     }
 }
